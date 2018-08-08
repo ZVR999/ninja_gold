3fb0a24cdfcd7e08ee1a8af83063719f55ccabd3
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-08-07 19:53:43 -0500

    Route /process_money added
    Imported random, added seesion['counter'], and route /process_money.
    Added /process_money and methods to all forms.
    Replaced 'Gold Counter' in  div.counter with session replacement to have a counter dynamically change when submitting forms.

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
-from flask import Flask, session, redirect, render_template
+from flask import Flask, session, redirect, render_template, request
+import random
 app = Flask(__name__)
 app.secret_key = 'n1312rn23!@123!%^VDSDSdgge5#$hfdss#$646u4'
 
 @app.route('/')
 def index():
+    if 'counter' not in session:
+        session['counter'] = 0
     return render_template('index.html')
 
+@app.route('/process_money', methods=['POST'])
+def process_money():
+    print request.form['building'] 
+
+    return redirect('/')
 app.run(debug=True)
