efb4645d06c5534f493ab4bf653dde280ec6c75e
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-08-08 11:04:15 -0500

    Date/Casino
    Formatted date and adding extra event when entering the casino. Where if you win/lose there will be a different response for each along with a different styling added.

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, session, redirect, render_template, request
-import random, datetime
+import random
+import datetime
 app = Flask(__name__)
 app.secret_key = 'n1312rn23!@123!%^VDSDSdgge5#$hfdss#$646u4'
+
 
 @app.route('/')
 def index():
@@ -10,43 +12,55 @@ def index():
         session['counter'] = 0
     return render_template('index.html')
 
+
 @app.route('/process_money', methods=['POST'])
 def process_money():
     building = request.form['building']
     print request.form['building']
     addAmount = 0
-    where = '' 
+    where = ''
     if building == 'farm':
         where = 'farm! '
-        addAmount = random.randint(10,20)
+        addAmount = random.randint(10, 20)
         session['counter'] += addAmount
     elif building == 'cave':
         where = 'cave! '
-        addAmount = random.randint(5,10)
+        addAmount = random.randint(5, 10)
         session['counter'] += addAmount
     elif building == 'house':
         where = 'house! '
-        addAmount = random.randint(2,5)
+        addAmount = random.randint(2, 5)
         session['counter'] += addAmount
     elif building == 'casino':
         where = 'casino! '
-        addAmount = random.randint(-50,50)
+        addAmount = random.randint(-50, 50)
         session['counter'] += addAmount
     if 'activity' not in session:
         session['activity'] = ''
     earned = '<div>Earned '
     addedAmount = str(addAmount)
     gold = ' golds from the '
-    # add where next
     dateEntered = datetime.datetime.now()
-    current = str(dateEntered.year)+'</div>'
-    session['activity'] += "{}{}{}{}{}".format(earned,addedAmount,gold,where,current)
+    current = '('+str(dateEntered.strftime('%x'))+' '+str(dateEntered.strftime('%I')
+                                                      )+':'+str(dateEntered.strftime('%M'))+')</div>'
+    if building == 'casino':
+        if addAmount >= 0:
+            earned = '<div>Entered a casino and won '
+            where += 'Nice! '
+        elif addAmount < 0:
+            earned = '<div>Entered a casino and lost '
+            where += '...Ouch.. '
+    session['activity'] += "{}{}{}{}{}".format(
+        earned, addedAmount, gold, where, current)
 
     return redirect('/')
+
 
 @app.route('/reset', methods=['POST'])
 def reset():
     session['counter'] = 0
     session['activity'] = ''
     return redirect('/')
+
+
 app.run(debug=True)
