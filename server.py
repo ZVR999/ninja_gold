ef498046d540203dd14605aedecc06ebd3001129
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-08-08 15:53:56 -0500

    session['array']/Reverse algo
    Added session['array'] to hold session['activity']'s history.
    Added a reverse algo to session['array'] to keep the new output in the Activities display box on the top rather than falling into the scroll.
    Works great!

--- server.py
+++ server.py
@@ -15,10 +15,13 @@ def index():
 
 @app.route('/process_money', methods=['POST'])
 def process_money():
+
     building = request.form['building']
-    print request.form['building']
+    #print request.form['building']
     addAmount = 0
     where = ''
+    
+    # Start of what to add to where and what to add to counter
     if building == 'farm':
         where = 'farm! '
         addAmount = random.randint(10, 20)
@@ -37,11 +40,15 @@ def process_money():
         session['counter'] += addAmount
     if 'activity' not in session:
         session['activity'] = ''
+    # End of what to add to where and what to add to counter
+    
+    # Start of what to add to session[activity]
     earned = '<div>Earned '
     addedAmount = str(addAmount)
     gold = ' golds from the '
     dateEntered = datetime.datetime.now()
     current = '('+str(dateEntered.strftime('%Y/%m/%d %I:%M %p'))+')</div>'
+
     if building == 'casino':
         if addAmount >= 0:
             earned = '<div class="green">Entered a casino and won '
@@ -49,8 +56,29 @@ def process_money():
         elif addAmount < 0:
             earned = '<div class="red">Entered a casino and lost '
             where += '...Ouch.. '
-    session['activity'] += "{}{}{}{}{}".format(
+    # End of what to add to session[activity]  
+    session['activity'] = "{}{}{}{}{}".format(
         earned, addedAmount, gold, where, current)
+    
+    # Create an array to hold session['activity']'s history
+    if 'array' not in session:
+        session['array'] = []
+    # Add session['activity']'s history to session['array']
+    session['array'] = list(session['array'])
+    session['array'].append(session['activity'])
+    # print session['array']
+    # print len(session['array'])
+    # Reverse the history in session['array'] so the last pops up on top of
+    # The Activites box
+    if len(session['array']) > 1:
+        def revese(arr):
+            for x in range(0,1):
+                arr.insert(0,arr[-1])
+                arr.pop()
+            return arr            
+
+        revese(session['array'])
+    session['array'] = ''.join(session['array'])
 
     return redirect('/')
 
@@ -59,6 +87,7 @@ def process_money():
 def reset():
     session['counter'] = 0
     session['activity'] = ''
+    session['array'] = ''
     return redirect('/')
 
 
