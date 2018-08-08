b69a4662f724b1b341e23b7d63fe6c6fb2f8d1e0
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-08-08 10:40:32 -0500

    Everything but date
    Almost completed assignment but need to format date shown to assignment format

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, session, redirect, render_template, request
-import random
+import random, datetime
 app = Flask(__name__)
 app.secret_key = 'n1312rn23!@123!%^VDSDSdgge5#$hfdss#$646u4'
 
@@ -14,25 +14,39 @@ def index():
 def process_money():
     building = request.form['building']
     print request.form['building']
-    addAmount = 0 
+    addAmount = 0
+    where = '' 
     if building == 'farm':
+        where = 'farm! '
         addAmount = random.randint(10,20)
         session['counter'] += addAmount
     elif building == 'cave':
+        where = 'cave! '
         addAmount = random.randint(5,10)
         session['counter'] += addAmount
     elif building == 'house':
+        where = 'house! '
         addAmount = random.randint(2,5)
         session['counter'] += addAmount
     elif building == 'casino':
+        where = 'casino! '
         addAmount = random.randint(-50,50)
         session['counter'] += addAmount
-
+    if 'activity' not in session:
+        session['activity'] = ''
+    earned = '<div>Earned '
+    addedAmount = str(addAmount)
+    gold = ' golds from the '
+    # add where next
+    dateEntered = datetime.datetime.now()
+    current = str(dateEntered.year)+'</div>'
+    session['activity'] += "{}{}{}{}{}".format(earned,addedAmount,gold,where,current)
 
     return redirect('/')
 
 @app.route('/reset', methods=['POST'])
 def reset():
     session['counter'] = 0
+    session['activity'] = ''
     return redirect('/')
 app.run(debug=True)
